9741275b14e8ba702709041912d8215f8710a93a
Dylan Merigaud <merigaudconsulting@gmail.com>
2025-01-25 19:23:21 +0100

    Finished 52 exercices 💪

--- exercises/quizzes/quiz2.rs
+++ exercises/quizzes/quiz2.rs
@@ -24,10 +24,22 @@ enum Command {
 }
 
 mod my_module {
+
     use super::Command;
 
     // TODO: Complete the function as described above.
-    // pub fn transformer(input: ???) -> ??? { ??? }
+    pub fn transformer(args: Vec<(String, Command)>) -> Vec<String> {
+        let mut results = Vec::new();
+        for (string, command) in args {
+            let transformed = match command {
+                Command::Uppercase => string.to_uppercase(),
+                Command::Trim => string.trim().to_string(),
+                Command::Append(n) => string + &"bar".repeat(n),
+            };
+            results.push(transformed);
+        }
+        results
+    }
 }
 
 fn main() {
@@ -38,6 +50,7 @@ fn main() {
 mod tests {
     // TODO: What do we need to import to have `transformer` in scope?
     // use ???;
+    use super::my_module::transformer;
     use super::Command;
 
     #[test]
